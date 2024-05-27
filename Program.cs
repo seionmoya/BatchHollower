90c35dfb39ab8813c1691dd685ffa09e18167cfb
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-05-27 23:10:19 +0200

    Initial project

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+
+namespace BatchHollower
+{
+    class Program
+    {
+        static readonly string[] _blacklist =
+        [
+            "Mono.Posix.dll",
+            "mscorlib.dll",
+            "System.Xml.Linq.dll",
+        ];
+
+        static void Main()
+        {
+            var files = new DirectoryInfo("./Managed")
+                .GetFiles()
+                .Where(x => !_blacklist.Contains(x.Name)
+                    && x.Extension == ".dll");
+
+            foreach (var fi in files)
+            {
+                var filepath = fi.FullName;
+
+                Console.WriteLine($"Hollowing {filepath}...");
+
+                try
+                {
+                    HollowAssembly(filepath);
+                }
+                catch (AssemblyResolutionException)
+                {
+                    Console.Error.WriteLine("WARNING: Cannot be resolved, might be obfuscated.");
+                }
+            }
+        }
+
+        static void HollowAssembly(string filepath)
+        {
+            var data = File.ReadAllBytes(filepath);
+            using var ms = new MemoryStream(data);
+            var module = ModuleDefinition.ReadModule(ms);
+
+            foreach (var type in module.Types)
+            {
+                if (!type.HasMethods)
+                {
+                    continue;
+                }
+
+                foreach (var method in type.Methods)
+                {
+                    try
+                    {
+                        if (method.HasBody)
+                        {
+                            method.Body.Instructions.Clear();
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        // Something gone wrong inside Mono.Cecil.
+                        // IL appears to be cleared.
+                    }
+                }
+            }
+
+            module.Write();
+            File.WriteAllBytes(filepath, ms.ToArray());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# BatchHollower
-Hollow multiple .NET assemblies
+# Batch Hollower
+
+Hollow multiple .NET assemblies at once.
+
+## Usage
+
+1. Drop your assemblies into `Managed/`
+2. `dotnet run`
